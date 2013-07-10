4d4288207b1f9d52b914c45d36b27147672ff108
Михаил Красильников <mihalych@vsepofigu.ru>
2013-07-10 11:46:34 +0400

    Прерывание выполнения файла при прямом вызове не имеет смысла, т. к. файл содержит только объявление класса.

--- action.php
+++ action.php
@@ -25,9 +25,6 @@
  * <http://www.gnu.org/licenses/>
  */
 
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 
