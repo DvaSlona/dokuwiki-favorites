38345552a910d9ecc09a5255eb81277238b894ff
Михаил Красильников <mihalych@vsepofigu.ru>
2013-07-10 10:55:24 +0400

    Прерывание выполнения файла при прямом вызове не имеет смысла, т. к. файл содержит только объявление класса.

--- syntax.php
+++ syntax.php
@@ -25,11 +25,6 @@
  * <http://www.gnu.org/licenses/>
  */
 
-// based on http://wiki.splitbrain.org/plugin:tutorial
-
-// must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
-
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 require_once(DOKU_PLUGIN . 'syntax.php');
 
