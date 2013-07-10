a2f958a66add2aea5d8db6028a3404a00617f344
Михаил Красильников <mihalych@vsepofigu.ru>
2013-07-10 10:45:00 +0400

    Изменена шапка файла.

--- syntax.php
+++ syntax.php
@@ -1,9 +1,28 @@
 <?php
 /**
- * favoris plugin : Affiche mes pages favorites (les plus visitees).
+ * Favorites plugin
  *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Etienne M. <emauvaisfr@yahoo.fr>
+ * Displays favorite pages. Based on Favoris plugin by Etienne M. <emauvaisfr@yahoo.fr>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.txt	GPL License 3
+ * @author Etienne M. <emauvaisfr@yahoo.fr>
+ * @author Михаил Красильников <mk@dvaslona.ru>
+ *
+ * Данная программа является свободным программным обеспечением. Вы
+ * вправе распространять ее и/или модифицировать в соответствии с
+ * условиями версии 3 либо (по вашему выбору) с условиями более поздней
+ * версии Стандартной Общественной Лицензии GNU, опубликованной Free
+ * Software Foundation.
+ *
+ * Мы распространяем эту программу в надежде на то, что она будет вам
+ * полезной, однако НЕ ПРЕДОСТАВЛЯЕМ НА НЕЕ НИКАКИХ ГАРАНТИЙ, в том
+ * числе ГАРАНТИИ ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ПРИГОДНОСТИ ДЛЯ
+ * ИСПОЛЬЗОВАНИЯ В КОНКРЕТНЫХ ЦЕЛЯХ. Для получения более подробной
+ * информации ознакомьтесь со Стандартной Общественной Лицензией GNU.
+ *
+ * Вы должны были получить копию Стандартной Общественной Лицензии
+ * GNU с этой программой. Если Вы ее не получили, смотрите документ на
+ * <http://www.gnu.org/licenses/>
  */
 
 // based on http://wiki.splitbrain.org/plugin:tutorial
@@ -258,4 +277,4 @@ class syntax_plugin_favoris extends DokuWiki_Syntax_Plugin {
      else return "<a href='doku.php?id=".$page."' class='wikilink2' style='font-weight: lighter;' title='$page".$title."' rel='nofollow'>$titrePage</a>";
    }
 }
-?>
+
